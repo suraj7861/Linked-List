19cb0e107e1bed6bccf98c14343d664b1404609c
unknown <khomanesuraj8@gmail.com>
2021-10-24 11:40:19 +0530

    merge uc3

--- src/com/dataStructure/MyLinkedList.java
+++ src/com/dataStructure/MyLinkedList.java
@@ -1,47 +1,46 @@
 package com.dataStructure;
+
 public class MyLinkedList {
-	//variable declaration
+	// variable declaration
 	Node head;
 	Node tail;
-	
-	//constructor
+
+	// constructor
 	public MyLinkedList() {
 		super();
 		this.head = null;
 		this.tail = null;
 	}
-	
-	//method: add node
+
+	// method: add node
 	public void add(int key) {
 		Node newNode = new Node(key);
 		if (head == null) {
 			head = newNode;
-		}
-		else {
+		} else {
 			Node tempNode = head;
 			head = newNode;
 			head.setNext(tempNode);
 		}
 	}
-	
-	//method: append node
-    public void append(int key) {
-    	Node newNode = new Node(key);
-        
-        if (tail==null){
-            tail=newNode;
-        }
-        else {
-            tail.setNext(newNode);
-            tail=newNode;
-        }
-    }
-    
-	//method: print node
+
+	// method: append node
+	public void append(int key) {
+		Node newNode = new Node(key);
+
+		if (tail == null) {
+			tail = newNode;
+		} else {
+			tail.setNext(newNode);
+			tail = newNode;
+		}
+	}
+
+	// method: print node
 	public void printNode() {
 		Node temp = head;
 		while (temp.getNext() != null) {
-			System.out.print(temp.key+"->");
+			System.out.print(temp.key + "->");
 			temp = temp.getNext();
 		}
 		System.out.print(temp.getKey());
